09cc51656c1ab51ee0eb85073f8ed559116e18fe
coscbui <coscbui@gmail.com>
2013-10-15 16:01:33 -0400

    adding upop for NOT

--- Code/ast.mli
+++ Code/ast.mli
@@ -1,7 +1,9 @@
-type operator = Add | Sub | Mul | Div | Mod | BeatAdd | BeatSub | PCAdd | PCSub |
-                BoolEq | Not | And | Or | Less | Leq | Greater | Geq | BeatLess | 
+type operator = Add | Sub | Mul | Div | Mod | BeatDiv | BeatMul | BeatAdd | BeatSub | PCAdd | PCSub |
+                BoolEq | And | Or | Less | Leq | Greater | Geq | BeatLess | 
                 BeatLeq | BeatGreater | BeatGeq | PCLess | PCLeq | PCGreater | 
                 PCGeq | Concat | Cons | Equal | TypeSpec | ArgTypeSpec
+
+type unary_operator = Not
 
 type row_operator = Inv | Retro | Trans
 
@@ -9,6 +11,7 @@ type expr =                               (* Expressions *)
   Literal of int                          (* 42 *)
   | Variable of string                    (* bar *)
   | Binop of expr * operator * expr       (* a + 2 *)
+  | Unop of unary_operator * expr         (* ! a == 4 *)
   | Rowop of row_operator * expr          (* ~[1,2,3,4,5,6] *)
   | Assign of string * expr               (* b = 4 *)
 
