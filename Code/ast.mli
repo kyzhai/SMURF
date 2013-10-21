5fff4d0e409cc3f7e439ca831e7ee62dd3a6f8e9
powerrmt <powerrmt@gmail.com>
2013-10-21 14:01:34 -0400

    Added declarations and patterns to ast.mli

--- Code/ast.mli
+++ Code/ast.mli
@@ -7,6 +7,9 @@ type unary_operator = Not
 
 type row_operator = Inv | Retro | Trans
 
+type types = Int | Bool | Note | Beat | Chord | System | [types]   (* Not sure if these should be here...doing it 
+                                                                         for type signature definition *)
+
 type expr =                                 (* Expressions *)
     Literal of int                          (* 42 *)
     | Variable of string                    (* bar *)
@@ -15,7 +18,22 @@ type expr =                                 (* Expressions *)
     | Rowop of row_operator * expr          (* ~[1,2,3,4,5,6] *)
 
 type dec =                                  (* Declarations *)
-    Def of string * expr                    (* b = 4 *)
+    Tysig of string * types list            (* f :: Int -> [Note] -> Bool *)
+    Funcdec of func_decl list               (* f x y = x + y *)
+
+type func_decl = {                          (* Function Declaration *)
+    fname : string;                         (* Function name *)
+    args : pattern list;                    (* Pattern arguments *)
+    value : expr;                           (* Expression bound to function *)
+}
+
+type pattern =                              (* Patterns *)
+    Patconst of int                         (* integer or boolean constant *)
+    Patvar of string                        (* identifier *)
+    Patlist of pattern list                 (* x:rest or [1,2,3] *)
+
+type def =                                  (* Definitions *)
+    Vardef of string * expr                 (* x = (2 + 5) : [1,2,3] *)
 
 type stmt =                                 (* Statements *)
     Expr of expr                            (* c = 4 : [3,2,1] *)
